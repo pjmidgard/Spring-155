d73c589df81e3476f314cb4a1c825868ccb52de7
Java Software <pj.midgard@yahoo.com>
2022-11-14 08:29:37 +0000

    Update Spring-155.py

--- Spring-155.py
+++ Spring-155.py
@@ -424,7 +424,7 @@ class compression:
                                                                         add_bits="0"+add_bits
                                                                         z=z+1
                                                         Equal_info_between_of_the_cirlce_of_the_file_17=add_bits+Equal_info_between_of_the_cirlce_of_the_file_17
-                                                print(Equal_info_between_of_the_cirlce_of_the_file_17)                                 
+                                                                             
                                                 if Extact==Equal_info_between_of_the_cirlce_of_the_file_17 and T!=0:
                                                     Equal_info_between_of_the_cirlce_of_the_file_17=Equal_info_between_of_the_cirlce_of_the_file2+Equal_info_between_of_the_cirlce_of_the_file3+Add_N+Info
       
