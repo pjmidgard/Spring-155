14671a1bc44972883c249b5124879ef7e69875f7
Java Software <pj.midgard@yahoo.com>
2022-11-23 18:50:13 +0000

    Update Spring-155.py

--- Spring-155.py
+++ Spring-155.py
@@ -364,7 +364,7 @@ class compression:
 
                                                 Hole_Number_information=(2**Deep5)-1
                                                 add_ones_together=Hole_Number_information                                                
-                                                Number_of_the_file=(Number_of_the_file+add_ones_together+Add)
+                                                Number_of_the_file=(Number_of_the_file+add_ones_together)+Add
                                                 
                                                 
                                                                                   
@@ -555,7 +555,7 @@ class compression:
                                                 
                                                 Hole_Number_information=(2**Deep5)-1
                                                 add_ones_together=Hole_Number_information      
-                                                Number_of_the_file=(Number_of_the_file+add_ones_together+Add)
+                                                Number_of_the_file=(Number_of_the_file+add_ones_together)+Add
                                                                                                 
                                                 
                                                 
