c8b7bd6c2207314e3c54e5d952c09bbb1c2b7155
Java Software <pj.midgard@yahoo.com>
2022-11-27 14:54:49 +0000

    Update Spring-155.py

--- Spring-155.py
+++ Spring-155.py
@@ -506,8 +506,8 @@ class compression:
                                             add_bitszzza=""
                                             add_bitszs=""
                                             Equal_info_between_of_the_cirlce_of_the_file_2=Times_6
-                                             import paq
-                                             width_bits3= paq.compress(width_bits3)
+                                            import paq
+                                            width_bits3= paq.compress(width_bits3)
                                                             
                                             with open(nameas, "wb") as f2:
                                                 f2.write(width_bits3)
