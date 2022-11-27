591a76649b909e8646559ee569bc90de93a55e0c
Java Software <pj.midgard@yahoo.com>
2022-11-27 19:35:22 +0000

    Update Spring-155.py

--- Spring-155.py
+++ Spring-155.py
@@ -108,9 +108,9 @@ class compression:
 
                        # Read the whole file at once
                         data = binary_file.read()
-                        #if i==2:
-                            #import paq
-                            #data= paq.decompress(data)
+                     
+                            
+                         
       
                         s=str(data)
 
@@ -527,8 +527,8 @@ class compression:
                                             add_bitszzza=""
                                             add_bitszs=""
                                             Equal_info_between_of_the_cirlce_of_the_file_2=Times_6
-                                            #import paq
-                                            #width_bits3= paq.compress(width_bits3)
+                                            
+                                  
                                                             
                                             with open(nameas, "wb") as f2:
                                                 f2.write(width_bits3)
